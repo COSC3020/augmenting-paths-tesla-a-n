7e2c07602ddaa48b3f413dbdc2579f6de142ea7d
tesla-a-n <143447224+tesla-a-n@users.noreply.github.com>
2025-05-12 22:26:01 -0600

    pcode

--- code.js
+++ code.js
@@ -1,3 +1,11 @@
 function augmentingPath(graph, start, end) {
+     // If start and end are the same, return just that node
+    // Queue for BFS
+    // Keep track of visited nodes
+    // Store the path
+    // BFS to find a path
+        // Check all neighbors of current node
+        // If we found the end node, reconstruct and return the path
+    // No path found
     return [];
 }
